6f16f8f1e8855e4efcc3a208e2a603942dcd3409
BryanGF0822 <bryanicesi0822@gmail.com>
2020-05-26 08:14:15 -0500

    Entrega Final del Proyecto

--- src/test/BallTest.java
+++ src/test/BallTest.java
@@ -1,35 +1,36 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-import model.*;
+import model.Ball;
+import model.Match;
 
 class BallTest {
 
 	private Ball ball;
 	private Match match;
-	
+
 	public void setup1() {
 		match = new Match(null, null, null, null);
 	}
-	
+
 	public void setup2() {
 		Ball b = new Ball(200, 200, "");
 	}
-	
+
 	@Test
 	void mover() {
 		setup1();
-		Ball b = new Ball(280,150,"");
+		Ball b = new Ball(280, 150, "");
 		match.setBall(b);
 		b.mover();
 		int x = b.getVectX();
 		int y = b.getVectY();
 		assertTrue(x == 10 && y == 10);
 		setup2();
-		Ball r = new Ball(265,100,"");
+		Ball r = new Ball(265, 100, "");
 		match.setBall(r);
 		r.mover();
 		int xv = r.getVectX();
